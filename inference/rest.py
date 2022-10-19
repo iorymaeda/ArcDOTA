bd43bf12ce613c5293586164e1be6353c64f7403
Maks <maksmoney87@gmail.com>
2022-10-20 01:51:02 +0500

    fix

--- inference/rest.py
+++ inference/rest.py
@@ -83,14 +83,9 @@ async def predict_prematch(team1:int, team2:int, key:str, match_id:int|None=None
 async def hawk_get_matches(date:str):
     try:
         chrome_options = uc.ChromeOptions()
-        chrome_options.add_argument("--remote-debugging-port=9222")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-using")
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("start-maximized")
-        chrome_options.add_argument("disable-infobars")
-        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('start-maximized')
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option('useAutomationExtension', False)
 
         driver = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver', options=chrome_options)
 
