022bce1b0245ed20e23500efc9564ec4007b70c5
Maks <maksmoney87@gmail.com>
2022-10-29 01:30:48 +0500

    fix

--- inference/rest.py
+++ inference/rest.py
@@ -108,10 +108,13 @@ async def hawk_get_matches(steamid:str):
             request.headers['Host'] = 'steamcommunity.com'
 
         options = Options()
-        #options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        #options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('window-size=1920x1080');
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument("start-maximized")
+        options.add_argument("disable-infobars")
+        options.add_argument("--disable-extensions")
+        options.add_argument("window-size=1920x1080")
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
         driver.request_interceptor = interceptor
