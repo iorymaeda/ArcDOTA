4df4d18bde6883e1e9ca643a29c2795bed4edfc3
Maks <maksmoney87@gmail.com>
2022-10-29 01:24:50 +0500

    fix

--- inference/rest.py
+++ inference/rest.py
@@ -13,7 +13,8 @@ import seleniumwire.undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdrivermanager.chrome import ChromeDriverManage
+
+from webdriver_manager.chrome import ChromeDriverManager
 
 import time
 import requests
