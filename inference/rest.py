40b8f755035989f501e257e8a883b3b33d9c26a4
Maks <maksmoney87@gmail.com>
2022-10-29 00:38:17 +0500

    fix

--- inference/rest.py
+++ inference/rest.py
@@ -102,11 +102,12 @@ async def hawk_get_matches(steamid:str):
             request.headers['Host'] = 'steamcommunity.com'
 
         options = uc.ChromeOptions()
-        prefs = {"profile.managed_default_content_settings.images": 2}
-        options.add_experimental_option("prefs", prefs)
+#         prefs = {"profile.managed_default_content_settings.images": 2}
+#         options.add_experimental_option("prefs", prefs)
 
         options.add_argument('-headless')
         options.add_argument('window-size=1920x1080')
+        options.add_argument('–no-sandbox --disable-dev-shm-usage')
         options.add_argument('--remote-debugging-port=9222')
        # options.binary_location = '/usr/bin/google-chrome-stable'
 
