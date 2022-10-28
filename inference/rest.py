d6d98088b7af4610197459557c9e0c3cf84e52ee
Maks <maksmoney87@gmail.com>
2022-10-29 00:49:27 +0500

    fix

--- inference/rest.py
+++ inference/rest.py
@@ -107,7 +107,8 @@ async def hawk_get_matches(steamid:str):
 
         options.add_argument('-headless')
         options.add_argument('window-size=1920x1080')
-        options.add_argument('–no-sandbox --disable-dev-shm-usage')
+        options.add_argument('–no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--remote-debugging-port=9222')
        # options.binary_location = '/usr/bin/google-chrome-stable'
 
